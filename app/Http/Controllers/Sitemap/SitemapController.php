17692cd72978e373ff129f4858345c209570c202
Евгений <112827365+get-route@users.noreply.github.com>
2022-11-02 01:53:39 +0300

    Update SitemapController.php

--- app/Http/Controllers/Sitemap/SitemapController.php
+++ app/Http/Controllers/Sitemap/SitemapController.php
@@ -9,6 +9,8 @@ use App\Models\Engine;
 
 class SitemapController extends Controller
 {
+    protected $count_auto;
+    protected $count_engine;
     public function index()
     {
 
@@ -18,18 +20,22 @@ class SitemapController extends Controller
     public function store(){
         $engines_sitemap = $this->data_engine();
         $automobiles_sitemap = $this->data_automobiles();
-        return view('frontend.sitemap.update',compact('engines_sitemap','automobiles_sitemap'));
+        $count_auto = $this->count_auto;
+        $count_engine = $this->count_engine;
+        return view('frontend.sitemap.update',compact('engines_sitemap','automobiles_sitemap','count_auto','count_engine'));
     }
 
     public function data_automobiles()
     {
         $autpmobiles = Automobiles::all('updated_at', 'url')->toArray();
-        return $this->sitemap_template($autpmobiles, 3000, 'url', 'updated_at', 'models_sitemap');
+        $this->count_auto = count($autpmobiles);
+        return $this->sitemap_template($autpmobiles, 3000, 'url', 'updated_at', 'models_sitemap','/car');
     }
 
     public function data_engine()
     {
         $engines = Engine::all('updated_at', 'slug')->toArray();
+        $this->count_engine = count($engines);
          return $this->sitemap_template($engines,3000,'slug','updated_at','engine_sitemap');
     }
 //    Для работы с генератором карты сайта необходимо указать: $input_dat - переменную где хранится выгрузка из БД
@@ -39,12 +45,13 @@ class SitemapController extends Controller
 //$url - The name of the field in the database to request the url
 //$date - The name of the field in the database to request the date
 //$name_file - The name of the final file after processing
-    public function sitemap_template($input_data, $paginate, String $url, String $date, String $name_file)
+    public function sitemap_template($input_data, $paginate, String $url, String $date, String $name_file,$category = null)
     {
 
         $page_number = $paginate;
+        $steps = 3000;
         $page_start = 0;
-        $step = (count($input_data) / $page_number) + 1;
+        $step = (count($input_data) / $page_number) + 1;//10
         for ($sitemap = 1; $sitemap <= $step; $sitemap++) {
 
 
@@ -53,7 +60,7 @@ class SitemapController extends Controller
             $urlset->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
 
 
-            for ($i = $page_start; $i < $page_number; $i++) {
+            for ($i = $page_start; $i < $page_number; $i++) {//0-3000
                 if (!empty($input_data[$i])) {
 
                     //Формируем валидную дату
@@ -64,7 +71,7 @@ class SitemapController extends Controller
                     // Элемент <loc> - URL статьи.
                     $loc = $dom->createElement('loc');
                     $text = $dom->createTextNode(
-                        route('index') . "/" . $input_data[$i]["$url"]
+                        route('index').$category . "/" . $input_data[$i]["$url"]
                     );
                     $loc->appendChild($text);
                     $urls->appendChild($loc);
@@ -90,7 +97,7 @@ class SitemapController extends Controller
             $dom->appendChild($urlset);
             $dom->save('./sitemap/' . $name_file . $sitemap . '.xml');
             $page_start = $page_number;
-            $page_number = $page_number + $page_start;
+            $page_number = $page_number + $steps;
         }
     }
 
