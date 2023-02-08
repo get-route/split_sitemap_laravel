f107a011a98fc12d04c0d0681ac33b052a72a798
evgpol <112827365+get-route@users.noreply.github.com>
2023-02-08 03:33:37 +0300

    getroute
    add update page views for sitemap

---
+++ resources/views/frontend/sitemap/update.blade.php
@@ -0,0 +1,67 @@
+@include('admin.layout.footer')
+@include('admin.layout.sidebar')
+@section('title','Обновление карты сайта')
+@include('admin.layout.header')
+    <!DOCTYPE html>
+<html lang="en">
+@yield('header')
+<!--
+`body` tag options:
+
+  Apply one or more of the following classes to to the body tag
+  to get the desired effect
+
+  * sidebar-collapse
+  * sidebar-mini
+-->
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+
+        </ul>
+
+        <!-- Right navbar links -->
+        <ul class="navbar-nav ml-auto">
+
+            <li class="nav-item">
+                <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                    <i class="fas fa-expand-arrows-alt"></i>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.navbar -->
+
+@yield('sidebar')
+
+<!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container">
+                <!-- /.row -->
+               <h1 class="text-center"> Обновление карты сайта</h1>
+            <p>Добавлено моделей авто - {{$count_auto}}
+        </p>
+                <p>Добавлено страниц двигателей - {{$count_engine}}</p>
+                <p>Посмотреть готовую карту сайта можно сдесь - <a href="{{route('sitemap.index')}}" class="btn btn-dark" target="_blank">Открыть</a></p>
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+
+    @yield('footer')
+</div>
+
+</body>
+</html>
